5feb075f6a05b5ccd1202c892e24968ef61ce778
Anurag Chandel <76684963+AnuragDOES@users.noreply.github.com>
2026-07-22 10:37:28 +0530

    Time: 0 ms (100%), Space: 77 MB (93.61%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,53 +1,27 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* res = new ListNode();
+        ListNode* dummy = new ListNode(-1); 
+        ListNode* temp = dummy;
         int carry = 0;
-        ListNode* temp = res;
-        while(l1 and l2){
-            ListNode* node = new ListNode();
-            temp->next = node;
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int val1 = (l1 != nullptr) ? l1->val : 0;
+            int val2 = (l2 != nullptr) ? l2->val : 0;
+
+            // Calculate the sum and the new carry
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+
+            // Create the new node with the single digit and attach it
+            temp->next = new ListNode(sum % 10);
             temp = temp->next;
-            int sum = l1->val + l2->val + carry;
-            temp->val = sum%10;
-            carry = sum/10;
-            l1 = l1->next;
-            l2 = l2->next;
+
+            // Safely advance the pointers if they aren't already null
+            if (l1 != nullptr) l1 = l1->next;
+            if (l2 != nullptr) l2 = l2->next;
         }
-        while(l1){
-            ListNode* node = new ListNode();
-            temp->next = node;
-            temp = temp->next;
-            int sum = l1->val + carry;
-            temp->val = sum%10;
-            carry = sum/10;
-            l1 = l1->next;
-        }
-        while(l2){
-            ListNode* node = new ListNode();
-            temp->next = node;
-            temp = temp->next;
-            int sum = l2->val + carry;
-            temp->val = sum%10;
-            carry = sum/10;
-            l2 = l2->next;
-        }
-        if(carry){
-            ListNode* node = new ListNode();
-            temp->next = node;
-            temp = temp->next;
-            temp->val = carry;
-        }
-        return res->next;
+
+        return dummy->next; // Return the actual head of the new list
     }
 };
